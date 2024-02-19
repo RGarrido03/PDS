cf81d7567c11c0cce875ec916dfa4833c7f09af7
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-19 17:03:01 +0000

    Use Scanner, simplify exception handling

--- lab01/SopaDeLetras.java
+++ lab01/SopaDeLetras.java
@@ -1,46 +1,41 @@
 package lab01;
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class SopaDeLetras {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         char[][] puzzle;
         String[] targetWords;
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("lab01/sopaDeLetras.txt"));
-            String line = reader.readLine();
+        Scanner reader = new Scanner(new FileReader("sopaDeLetras.txt"));
+        String line = reader.nextLine();
 
-            while (isUpperCase(line)) {
-                puzzle = new char[line.length()][line.length()];
+        while (isLineUpperCase(line)) {
+            puzzle = new char[line.length()][line.length()];
 
-                for (int i = 0; i < line.length(); i++) {
-
-                    if (puzzle.length == puzzle[0].length) {
-                        // insert in bidimensional array
-                        puzzle[i] = line.toCharArray();
-                        System.out.println(puzzle);
-                        // read next line
-                        line = reader.readLine();
-                    } else {
-                        System.out.println("Sopa de Letras inválida!");
-                    }
+            for (int i = 0; i < line.length(); i++) {
+                if (puzzle.length == puzzle[0].length) {
+                    // insert in bidimensional array
+                    puzzle[i] = line.toCharArray();
+                    System.out.println(puzzle);
+                    // read next line
+                    line = reader.nextLine();
+                } else {
+                    System.out.println("Sopa de Letras inválida!");
                 }
             }
-
-            while (!isUpperCase(line)) {
-                System.out.println(line);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        while (!isLineUpperCase(line)) {
+            System.out.println(line);
+        }
+        reader.close();
 
     }
 
-    static boolean isUpperCase(String line) {
+    static boolean isLineUpperCase(String line) {
         for (char c : line.toCharArray()) {
             if (Character.isLowerCase(c)) {
                 return false;
