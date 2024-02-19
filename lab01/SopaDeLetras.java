466cf72ac8f07a8746852dce125094702113014d
Bruno Lopes <63374228+Brupez@users.noreply.github.com>
2024-02-19 16:46:14 +0000

    txt file and initial SopadeLetras

---
+++ lab01/SopaDeLetras.java
@@ -0,0 +1,63 @@
+package lab01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SopaDeLetras {
+
+
+    public static void main(String[] args) {
+
+        char[][] puzzle;
+        String[] targetWords;
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("lab01/sopaDeLetras.txt"));
+            String line = reader.readLine();
+
+            while (line != null && isUpperCase(line) == true) {
+
+                puzzle = new char[line.length()][line.length()];
+
+                for(int i=0; i< line.length(); i++){
+
+                    if (puzzle.length == puzzle[0].length){
+
+                        // insert in bidimensional array
+                        puzzle[i] = line.toCharArray();
+                        System.out.println(puzzle);
+                        // read next line
+                        line = reader.readLine();  
+    
+                    } else {
+                        System.out.println("Sopa de Letras inválida!");
+                    }  
+                }
+               
+            }
+
+            while (isUpperCase(line) == false) {
+                
+                System.out.println(line);
+                
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static boolean isUpperCase(String line) {
+
+        for (char c : line.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
