c6c3ae724778cc8226d9cbcae3a6d2e9c71d6b2f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-23 19:42:45 +0000

    Remove unused Utils methods

--- lab01/Utils.java
+++ lab01/Utils.java
@@ -1,25 +1,9 @@
 package lab01;
 
-import java.io.PrintWriter;
 import java.util.Random;
 
 public class Utils {
     private static final Random RANDOM = new Random();
-
-    public static char[][] initializePuzzle(int size) {
-        char[][] puzzle = new char[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                puzzle[i][j] = '.';
-            }
-        }
-        return puzzle;
-    }
-
-    public static void printToConsoleAndFile(String str, PrintWriter writer) {
-        writer.println(str);
-        System.out.println(str);
-    }
 
     public static boolean isNumeric(String str) {
         try {
