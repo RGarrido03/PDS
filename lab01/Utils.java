09245fe2403babbfe18999961962ce482d7e237b
Brupez <63374228+Brupez@users.noreply.github.com>
2024-02-24 18:23:05 +0000

    javadoc Utils class

--- lab01/Utils.java
+++ lab01/Utils.java
@@ -5,6 +5,11 @@ import java.util.Random;
 public class Utils {
     private static final Random RANDOM = new Random();
 
+    /**<b>Check if string has numeric elements</b>
+     *
+     * @param str
+     * @return str numeric or not
+     */
     public static boolean isNumeric(String str) {
         try {
             Integer.parseInt(str);
@@ -14,19 +19,42 @@ public class Utils {
         return true;
     }
 
+    /**<b> Check if line has UpperCase letters</b>
+     *
+     * @param line
+     * @return line is UpperCase or not
+     */
     public static boolean isLineUpperCase(String line) {
         return line.equals(line.toUpperCase());
     }
 
+    /**<b> Set Random Direction</b>
+     * <p>
+     *     Used in WSGenerator to set random direction to each new element
+     * </p>
+     * @return direction coordinates
+     */
     public static Direction randomDirection() {
         int x = RANDOM.nextInt(Direction.values().length);
         return Direction.values()[x];
     }
 
+    /**
+     * <b>Random Char</b>
+     * <p>
+     *     Used in WSGenerator to set a random character to each new element
+     * </p>
+     * @return
+     */
     public static char randomChar() {
         return (char) (RANDOM.nextInt(26) + 'A');
     }
 
+    /**
+     * <b> Turns first element in a String to UpperCase</b>
+     * @param string
+     * @return String with first element in UpperCase
+     */
     public static String capitalizeString(String string) {
         return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
