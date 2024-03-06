564b87223956b82b0786b495fab587d4d42cf713
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-06 00:31:43 +0000

    Create exercise structure (meh, let's see)

---
+++ lab03/ex2/Flight.java
@@ -0,0 +1,6 @@
+package lab3.ex2;
+
+public class Flight {
+    private String id;
+    private Plane plane;
+}
---
+++ lab03/ex2/TouristicPlane.java
@@ -0,0 +1,6 @@
+package lab3.ex2;
+
+public class TouristicPlane implements Plane {
+    private int seatLines;
+    private int seatsPerLine;
+}
