a8d2b15592e4427c30b1bc624f629c6bcf7b6ecf
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-16 22:46:45 +0100

    Implement missing methods in AdvancedPrinter

--- lab07/ex3/AdvancedPrinter.java
+++ lab07/ex3/AdvancedPrinter.java
@@ -1,5 +1,7 @@
 package ex3;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.*;
 
 public class AdvancedPrinter implements AdvancedPrinterInterface {
@@ -9,6 +11,42 @@ public class AdvancedPrinter implements AdvancedPrinterInterface {
     public AdvancedPrinter() {
         this.spool = new PrinterService();
         new Thread(this.spool).start();
+    }
+
+    @Override
+    public int print(Document doc) {
+        System.out.println("Spooling 1 document.");
+        return spool.newPrintJob(doc);
+    }
+
+    @Override
+    public List<Integer> print(List<Document> docs) {
+        System.out.println("Spooling " + docs.size() + " documents.");
+        return docs.stream().map(spool::newPrintJob).toList();
+    }
+
+    @Override
+    public void showQueuedJobs() {
+        if (spool.getPrintQueue().isEmpty()) {
+            System.out.println("No spooled jobs.");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Spooled jobs:\n");
+        spool.getPrintQueue().stream().map(printJob -> "  * " + printJob.toString() + "\n").forEach(sb::append);
+        System.out.println(sb);
+    }
+
+    @Override
+    public boolean cancelJob(int jobId) {
+        return spool.cancelJob(jobId);
+    }
+
+    @Override
+    public void cancelAll() {
+        spool.shutdownAndAwaitTermination();
+        spool.printQueue.clear();
     }
 
     // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/concurrent/ExecutorService.html
@@ -78,8 +116,4 @@ public class AdvancedPrinter implements AdvancedPrinterInterface {
             return printQueue;
         }
     }
-
-    // TODO: implementar métodos
-
-
 }
