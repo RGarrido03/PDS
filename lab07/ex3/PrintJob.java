fdf04776b0b94c6871d7396f70f34ba173c475d7
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-16 21:48:52 +0100

    Add id to PrintJob

--- lab07/ex3/PrintJob.java
+++ lab07/ex3/PrintJob.java
@@ -3,15 +3,23 @@ package ex3;
 import java.util.concurrent.Callable;
 
 public class PrintJob implements Callable<Document> {
+    private static int nextJobId = 0;
+
+    private final int jobId;
     private final Document document;
 
     public PrintJob(Document doc) {
+        this.jobId = nextJobId++;
         this.document = doc;
     }
 
     @Override
     public Document call() {
         return document;
+    }
+
+    public int getJobId() {
+        return jobId;
     }
 
     @Override
