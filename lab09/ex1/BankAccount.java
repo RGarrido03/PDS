f8314784a756ebd5aa96c77bc48cf4e6033788d2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-06 16:24:41 +0100

    Add initial ex09 files

---
+++ lab09/ex1/BankAccount.java
@@ -0,0 +1,8 @@
+package ex1;
+
+interface BankAccount {
+	void deposit(double amount);
+	boolean withdraw(double amount);
+	double balance();
+}
+
---
+++ lab09/ex3/Sky.java
@@ -0,0 +1,24 @@
+package ex3;
+
+import javax.swing.*;
+
+import ex3.startypes.StarType;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sky extends JFrame {
+    private List<StarType> stars = new ArrayList<>();
+
+    public void placeStar(StarType star) {
+        stars.add(star);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (StarType star : stars) {
+            star.draw(graphics);
+        }
+    }
+}
